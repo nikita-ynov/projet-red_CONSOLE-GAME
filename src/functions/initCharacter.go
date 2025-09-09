4720d909d1d311bdc9b6f58fe3edb745259fd636
Shibler0 <rischmartin@laposte.net>
2025-09-09 10:45:14 +0200

    new func initcharacter created

--- src/functions/initCharacter.go
+++ src/functions/initCharacter.go
@@ -5,19 +5,19 @@ import (
 	"fmt"
 )
 
-func InitCharacter() structure.Character {
-	// Example
-	var obj1 structure.Inventory = structure.Inventory{Name: "Bubble Gum", ChangeHp: 2, Quantity: 3}
-	fmt.Println(obj1)
-	// {Bubble Gum 2} {Phone 1}
+func InitCharacter(u structure.Character, obj []structure.Inventory) structure.Character {
+	// // Example
+	// var obj1 structure.Inventory = structure.Inventory{Name: "Bubble Gum", ChangeHp: 2, Quantity: 3}
+	// fmt.Println(obj1)
+	// // {Bubble Gum 2} {Phone 1}
 
 	var player structure.Character = structure.Character{
-		Name:      "Tom",
-		Class:     "SS+",
-		Lvl:       1,
-		HpMax:     100,
-		CurrentHp: 70,
-		Inventory: []structure.Inventory{obj1},
+		Name:      u.Name,
+		Class:     u.Class,
+		Lvl:       u.Lvl,
+		HpMax:     u.HpMax,
+		CurrentHp: u.CurrentHp,
+		Inventory: obj,
 	}
 	fmt.Println(player)
 	return player
