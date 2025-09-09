36b94b25822508280987185cde26e1ed38165297
Nikita Petrenko <nikita.petrenko@ynov.com>
2025-09-09 10:43:47 +0200

    add displayInfo

--- src/functions/initCharacter.go
+++ src/functions/initCharacter.go
@@ -2,14 +2,11 @@ package functions
 
 import (
 	structure "PROJETRED/structure"
-	"fmt"
 )
 
 func InitCharacter() structure.Character {
 	// Example
-	var obj1 structure.Inventory = structure.Inventory{Name: "Bubble Gum", ChangeHp: 2, Quantity: 3}
-	fmt.Println(obj1)
-	// {Bubble Gum 2} {Phone 1}
+	var obj1 structure.Inventory = structure.Inventory{Name: "Bubble Gum", ChangeHp: -2, Quantity: 3}
 
 	var player structure.Character = structure.Character{
 		Name:      "Tom",
@@ -19,6 +16,5 @@ func InitCharacter() structure.Character {
 		CurrentHp: 70,
 		Inventory: []structure.Inventory{obj1},
 	}
-	fmt.Println(player)
 	return player
 }
--- src/main.go
+++ src/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	functions "PROJETRED/functions"
-	structure "PROJETRED/structure"
 	"fmt"
 )
 
-var character structure.Character
-
 func main() {
 	fmt.Println("START GAME")
-	functions.InitCharacter()
+	player := functions.InitCharacter()
+	functions.DisplayInfo(player)
 }
