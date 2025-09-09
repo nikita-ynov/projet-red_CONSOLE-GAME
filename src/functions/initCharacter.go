cac93d5ac22f304ee91b8818178d53da3b06abd5
Nikita Petrenko <nikita.petrenko@ynov.com>
2025-09-09 09:53:47 +0200

    add exmp init func

--- src/functions/initCharacter.go
+++ src/functions/initCharacter.go
@@ -1,7 +1,24 @@
 package functions
 
-import "fmt"
+import (
+	structure "PROJETRED/structure"
+	"fmt"
+)
 
 func InitCharacter() {
-	fmt.Println("Test func")
+	// Example
+	var obj1 structure.Inventory = structure.Inventory{Name: "Bubble Gum", ChangeHp: 2, Quantity: 3}
+	fmt.Println(obj1)
+	// {Bubble Gum 2} {Phone 1}
+
+	var player structure.Character = structure.Character{
+		Name:      "Tom",
+		Class:     "SS+",
+		Lvl:       1,
+		HpMax:     100,
+		CurrentHp: 70,
+		Inventory: []structure.Inventory{obj1},
+	}
+
+	fmt.Println(player)
 }
