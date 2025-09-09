b44b1440e85fe71a4531240f0f616c32b823b230
Nikita Petrenko <nikita.petrenko@ynov.com>
2025-09-09 15:14:36 +0200

    fix display

--- src/functions/displayInfo.go
+++ src/functions/displayInfo.go
@@ -9,25 +9,20 @@ func DisplayInfo(player *structure.Character) {
 	fmt.Print("\033[H\033[2J")
 	fmt.Println("====== DISPLAY INFO - " + player.Name + " ======")
 	fmt.Println("Class: " + player.Class)
-	fmt.Printf("Lvl: %v", player.Lvl)
-	fmt.Printf("\n")
-	fmt.Printf("Hp Max: %v", player.HpMax)
-	fmt.Printf("\n")
-	fmt.Printf("Current Hp: %v", player.CurrentHp)
-	fmt.Printf("\n")
-	fmt.Printf("INVENTORY ITEMS: %v", len(player.Inventory))
-	fmt.Printf("\n")
+	fmt.Printf("Lvl: %v\n", player.Lvl)
+	fmt.Printf("Hp Max: %v\n", player.HpMax)
+	fmt.Printf("Current Hp: %v\n", player.CurrentHp)
+	fmt.Printf("INVENTORY ITEMS: %v\n", len(player.Inventory))
 	for _, item := range player.Inventory {
 		fmt.Println("------------")
 		fmt.Println(item.Name)
 		fmt.Printf("Quantity: %v", item.Quantity)
 		fmt.Printf("\n")
 		if item.ChangeHp < 0 {
-			fmt.Printf("Damage Hp: %v", item.ChangeHp)
+			fmt.Printf("Damage Hp: %v\n", item.ChangeHp)
 		} else if item.ChangeHp > 0 {
-			fmt.Printf("Health Hp: %v", item.ChangeHp)
+			fmt.Printf("Health Hp: %v\n", item.ChangeHp)
 		}
-		fmt.Printf("\n")
 		fmt.Println("------------")
 	}
 
