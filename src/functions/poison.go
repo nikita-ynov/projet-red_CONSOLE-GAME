c42955be75cba206b8001dc677e9d5b0902889cd
Kevin <kevin.joffret@gmail.com>
2025-09-09 16:26:42 +0200

    added damage of Potion of poison

---
+++ src/functions/poison.go
@@ -0,0 +1,16 @@
+package functions
+
+import (
+	structure "PROJETRED/structure"
+	"fmt"
+	"time"
+)
+
+func PoisonPot(player *structure.Character) {
+	for i := 0; i < 3; i++ {
+		fmt.Println("Damage of poison : 10 \n")
+		fmt.Println("current Health :")
+		fmt.Println(player.CurrentHp - 10)
+		time.Sleep(1 * time.Second)
+	}
+}
